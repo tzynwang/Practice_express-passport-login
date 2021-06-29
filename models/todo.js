f515f3a66e27e0ebaa7c54d3261c5262557d16b9
tzyn (Windows Fork) <tzyn.wang@gmail.com>
2021-06-29 10:39:37 +0800

    feat: add todoSchema

---
+++ models/todo.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const todoSchema = new Schema({
+  userId: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  isDone: {
+    type: Boolean,
+    default: false,
+    require: true
+  },
+  createdDate: {
+    type: Number,
+    required: true
+  },
+  endDate: {
+    type: Number,
+    trim: true
+  },
+  duration: {
+    type: Number
+  },
+  isDelete: {
+    type: Boolean,
+    default: false,
+    require: true
+  }
+})
+
+module.exports = mongoose.model('Todo', todoSchema)
